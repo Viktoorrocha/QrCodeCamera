2710666696b61259d61cda16ee566240212caa13
Viktor Rocha <viktoorrocha@gmail.com>
2017-09-15 22:49:37 -0400

    First

---
+++ app/src/main/java/qrcode/br/com/qrcodecamera/MainActivity.java
@@ -0,0 +1,35 @@
+package qrcode.br.com.qrcodecamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+public class MainActivity extends AppCompatActivity {
+
+    SurfaceView cameraPreview;
+    TextView txtResult;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    int RequestCameraPermissionID = 1001;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        cameraPreview = (SurfaceView)findViewById(R.id.cameraPreview);
+        txtResult = (TextView)findViewById(R.id.txtResult);
+
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+    }
+}
