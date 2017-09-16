b49b052a4de3582e4f7f448d07e7c2d8555df864
Viktor Rocha <viktoorrocha@gmail.com>
2017-09-15 23:15:22 -0400

    Second

--- app/src/main/java/qrcode/br/com/qrcodecamera/MainActivity.java
+++ app/src/main/java/qrcode/br/com/qrcodecamera/MainActivity.java
@@ -1,13 +1,18 @@
 package qrcode.br.com.qrcodecamera;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     CameraSource cameraSource;
     int RequestCameraPermissionID = 1001;
 
+    @Override
+    public void OnRequestPermissionsResult(int )
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +33,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        cameraPreview = (SurfaceView)findViewById(R.id.cameraPreview);
-        txtResult = (TextView)findViewById(R.id.txtResult);
+        cameraPreview = (SurfaceView) findViewById(R.id.CameraPreview);
+        txtResult = (TextView) findViewById(R.id.textResult);
 
 
         barcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.QR_CODE)
                 .build();
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[] {Manifest.permission.CAMERA},RequestCameraPermissionID);
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                    cameraSource.stop();
+            }
+        });
     }
 }
